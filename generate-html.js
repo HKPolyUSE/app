3c503fd267db1d343fe974e02939a96a814f4438
cswclui <218137+cswclui@users.noreply.github.com>
2026-02-23 00:09:53 +0800

    Update HTML generation to write files to root directory

--- generate-html.js
+++ generate-html.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 
 const srcDir = path.resolve(__dirname, 'src');
+const rootDir = path.resolve(__dirname);  // ← write HTML to root
 const repoName = 'app';
 const orgName = 'hkpolyuse';
+
 const files = fs.readdirSync(srcDir).filter(f => f.endsWith('.jsx'));
 
-// Generate HTML files
 files.forEach(file => {
   const name = path.basename(file, '.jsx');
   const html = `<!DOCTYPE html>
@@ -14,25 +15,19 @@ files.forEach(file => {
   <head><title>${name}</title></head>
   <body>
     <div id="root"></div>
-    <script type="module" src="./${file}"></script>
+    <script type="module" src="./src/${file}"></script>
   </body>
 </html>`;
-  fs.writeFileSync(path.resolve(srcDir, `${name}.html`), html);
+  fs.writeFileSync(path.resolve(rootDir, `${name}.html`), html);  // ← root
   console.log(`Generated ${name}.html`);
 });
 
-// Generate README.md with all public links
+// Generate README
 const links = files.map(file => {
   const name = path.basename(file, '.jsx');
   return `- [${name}](https://${orgName}.github.io/${repoName}/${name}.html)`;
 }).join('\n');
 
-const readme = `# App Components
-
-## Live Pages
-
-${links}
-`;
-
-fs.writeFileSync(path.resolve(__dirname, 'README.md'), readme);
+const readme = `# App Components\n\n## Live Pages\n\n${links}\n`;
+fs.writeFileSync(path.resolve(rootDir, 'README.md'), readme);
 console.log('Generated README.md');
